7a1242fcfa9f8bd61720870543ce91225f9f99da
urvi2408 <bhavsarurvi143@gmail.com>
2025-03-20 12:09:39 +0530

    UI changes

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,58 +1,54 @@
-import React,{useState} from 'react'
-import Food from "../Food.json"
-import "../App.css"
-import FoodItem from './FoodItem'
+import React, { useState } from "react";
+import Food from "../Food.json";
+import "../App.css";
+import FoodItem from "./FoodItem";
 
 const Category = () => {
-
-
-  const [data,setData] = useState(Food);
-  console.log("data from category",data);
+  const [data, setData] = useState(Food);
+  console.log("data from category", data);
 
   const filterItem = (catItem) => {
-      const newItem = Food.filter((x) => {
-        return  x.category === catItem;
-      });
-      setData(newItem);
+    const newItem = Food.filter((x) => {
+      return x.category === catItem;
+    });
+    setData(newItem);
   };
 
   return (
     <>
-    <div className='Category'>
-        <img className='category_btn1' 
-            src="https://fontmeme.com/temporary/a63804cef7e0aaf56e9f9d4576824e5e.png" 
-            onClick={() => setData(Food)} 
-            alt="#"
-        />
-        <img className='category_btn2' 
-            src="https://fontmeme.com/temporary/3dc69b7c0153af7a900eacc4134524f7.png" 
-            onClick={() => filterItem('Vegetables')}
-            alt="#"
-        />
-        <img className='category_btn2' 
-            src="https://fontmeme.com/temporary/590fb4f11a3c9cb02c78e03933c502bf.png"
-            onClick={() => filterItem('Drinks')}
-            alt="#"
-        />
-        <img className='category_btn3' 
-            src="https://fontmeme.com/temporary/98c23f619880226581414a0dc5959aa0.png"
-            onClick={() => filterItem('Dairy Items')}
-            alt="#"
-        />
-        <img className='category_btn4' 
-            src="https://fontmeme.com/temporary/f87bd3c6d1c4124c3557c4b19d167aec.png"
-            onClick={() => filterItem('Fruit')}
-            alt="#"
-        />
-        <img className='category_btn5' 
-            src="https://fontmeme.com/temporary/893d050770818da1e89306b8bd3826de.png"
-            onClick={() => filterItem('Nuts And Seeds')}
-            alt="#"
-        />
-    </div>
-    <FoodItem data={data}/>
-    </>
-  )
-}
+      <div className="Category">
+        <button className="category_btn1" onClick={() => setData(Food)}>
+          All
+        </button>
+        <button
+          className="category_btn2"
+          onClick={() => filterItem("Vegetables")}
+        >
+          Vegetables
+        </button>
+        <button className="category_btn2" onClick={() => filterItem("Drinks")}>
+          Drinks
+        </button>
+        <button
+          className="category_btn3"
+          onClick={() => filterItem("Dairy Items")}
+        >
+          Dairy Items
+        </button>
+        <button className="category_btn4" onClick={() => filterItem("Fruit")}>
+          Fruit
+        </button>
+        <button
+          className="category_btn5"
+          onClick={() => filterItem("Nuts And Seeds")}
+        >
+          Nuts & Seeds
+        </button>
+      </div>
 
-export default Category
+      <FoodItem data={data} />
+    </>
+  );
+};
+
+export default Category;
--- src/components/FoodItemInfo.js
+++ src/components/FoodItemInfo.js
@@ -1,42 +1,51 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import "../App.css"
+import React from "react";
+import { useParams } from "react-router-dom";
+import "../App.css";
 import Food from "../Food.json";
-import Header from './Header';
-import {useDispatch} from "react-redux";
-import { Divider } from 'antd';
-import 'antd/dist/antd.css';
-import {AddToCartItem} from "../Actions";
+import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { Divider } from "antd";
+import "antd/dist/antd.css";
+import { AddToCartItem } from "../Actions";
 
 const FoodItemInfo = () => {
-
   const dispatch = useDispatch();
 
-  const {id} = useParams();
+  const { id } = useParams();
   console.log(id);
 
-  const handleClickCart =  () => {
-      dispatch(AddToCartItem(Food[id]))
-  }
+  const handleClickCart = () => {
+    dispatch(AddToCartItem(Food[id]));
+  };
 
   return (
-    <>   
-    <Header/>
-    <div className='FoodItemInfo'>
-        <div className='leftimg'>
-           <img className=' ' src={Food[id]?.image} alt="#"/>
+    <>
+      <Header />
+      <div className="FoodItemInfo">
+        <div className="leftimg">
+          <img
+            style={{ height: "250px", width: "400px" }}
+            src={Food[id]?.image}
+            alt="#"
+          />
         </div>
-        <Divider type="vertical" style={{height:"500px"}}/>
-        <div className='iteminfo'>
-           <p ><b>{Food[id]?.name}</b></p>
-           <p className='category'>Category : {Food[id]?.category}</p>
-           <p className='description'>{Food[id]?.description}</p>
-           <p className='description'>INR {Food[id]?.price}</p>
-           <button className='infobtn' onClick={handleClickCart}>Add To Cart</button><br/><br/>
+        <Divider type="vertical" style={{ height: "500px" }} />
+        <div className="iteminfo">
+          <p>
+            <b>{Food[id]?.name}</b>
+          </p>
+          <p className="category">Category : {Food[id]?.category}</p>
+          <p className="description">{Food[id]?.description}</p>
+          <p className="description">INR {Food[id]?.price}</p>
+          <button className="infobtn" onClick={handleClickCart}>
+            Add To Cart
+          </button>
+          <br />
+          <br />
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default FoodItemInfo
+export default FoodItemInfo;
